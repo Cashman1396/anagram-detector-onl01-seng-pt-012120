40d017e54d9a267fedc35f22f0b84f8560571240
Chris Ashman <chrisashman96@gmail.com>
2020-02-06 21:50:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end 
   
   def match(array)
-    array.select{|x| x.split("").sort = @name.split("").sort}
+    array.select{|x| x.split("").sort == @name.split("").sort}
   end 
   
 end 
